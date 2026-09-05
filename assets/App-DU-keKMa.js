d58ea220f8a7cb2b664e1320ead07154d6a3c3a3
smartstratmedia <smartstratmedia@gmail.com>
2026-09-05 23:34:07 +0100

    Deploy crash recovery release a6e43bdaf

--- assets/App-DU-keKMa.js
+++
@@ -1,3 +0,0 @@
-// Compatibility module for browsers holding the final Lovable entry bundle.
-export * from "/assets/App-C5qEaZRF.js";
-
--- assets/index-0qKOduYB.js
+++
@@ -1,3 +0,0 @@
-// Compatibility entry for browsers holding the final Lovable HTML shell.
-import "/assets/index-CxPOSTFx.js";
-
